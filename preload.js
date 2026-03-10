f999af9d09af2cbe1a972d11ef39efeacd3920b9
Nandakumar Vadivelu <nanda@apache.org>
2026-03-10 22:42:48 +0530

    Added logic to edit stored snippet

--- preload.js
+++ preload.js
@@ -3,17 +3,12 @@ const { contextBridge, ipcRenderer } = require("electron");
 contextBridge.exposeInMainWorld("api", {
   search: (q) => ipcRenderer.invoke("snippets:search", q),
   recent: () => ipcRenderer.invoke("snippets:recent"),
-
   create: (payload) => ipcRenderer.invoke("snippets:create", payload),
   update: (payload) => ipcRenderer.invoke("snippets:update", payload),
   delete: (id) => ipcRenderer.invoke("snippets:delete", id),
-
   copy: (text) => ipcRenderer.invoke("clipboard:copy", text),
-
+  copyAndPaste: (text) => ipcRenderer.invoke("clipboard:copyAndPaste", text),
   hide: () => ipcRenderer.invoke("ui:hide"),
-
   onFocusSearch: (cb) => ipcRenderer.on("focus-search", cb),
-  
   quit: () => ipcRenderer.invoke("app:quit")
-
 });
